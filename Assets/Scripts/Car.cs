0b3884861c63a74d84f7f608c72b9a83e4ebb726
AlexChichibabin <Wolfrahm57@mail.ru>
2024-10-03 17:55:12 +0300

    Коробка передач 1

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -8,11 +8,27 @@ namespace Racing
         [SerializeField] private float maxSteerAngle;
         [SerializeField] private float maxBrakeTorque;
 
+        [Header ("Engine")]
         [SerializeField] private AnimationCurve engineTorqueCurve;
-        [SerializeField] private float maxMotorTorque;
+        [SerializeField] private float engineMaxTorque;
+        [SerializeField] private float engineTorque;
+        [SerializeField] private float engineRPM;
+        [SerializeField] private float engineMinRPM;
+        [SerializeField] private float engineMaxRPM;
+
+        [Header("Gearbox")]
+        [SerializeField] private float[] gears;
+        [SerializeField] private float finalDriveRatio;
+
+        [SerializeField] private float selectedGear;
+        [SerializeField] private float rearGear;
+        [SerializeField] private int selectedGearIndex;
+
         [SerializeField] private float maxSpeed;
 
         [SerializeField] private float handBrakeFactor = 1.0f;
+
+
         public float LinearVelocity => chassis.LinearVelocity;
         public float WheelSpeed => chassis.GetWheelSpeed();
         public float MaxSpeed => maxSpeed;
@@ -42,7 +58,9 @@ namespace Racing
         }
         private void Update()
         {
-            float engineTorque = engineTorqueCurve.Evaluate( LinearVelocity / maxSpeed ) * maxMotorTorque;
+            UpdateEngineTorque();
+
+            //float engineTorque = engineTorqueCurve.Evaluate( LinearVelocity / maxSpeed ) * engineMaxTorque; 
 
             for (int i = 0; i < 2; i++)
             {
@@ -55,6 +73,31 @@ namespace Racing
             chassis.SteerAngle = SteerControl * maxSteerAngle;
             chassis.BrakeTorque = BrakeControl * maxBrakeTorque;
             chassis.HandBrakeTorque = HandBrakeControl * maxBrakeTorque * handBrakeFactor;
+        }
+
+        private void UpdateEngineTorque()
+        {
+            engineRPM = engineMinRPM + Mathf.Abs(chassis.GetAverageRpm() * selectedGear * finalDriveRatio);
+            engineRPM = Mathf.Clamp(engineRPM, engineMinRPM, engineMaxRPM);
+
+            engineTorque = engineTorqueCurve.Evaluate(engineRPM / engineMaxRPM) * engineMaxTorque * finalDriveRatio * Mathf.Sign(selectedGear * gears[0]);
+        }
+
+        //GearBox
+        private void ShiftGear(int gearIndex)
+        {
+            gearIndex = Mathf.Clamp(gearIndex, 0, gears.Length - 1);
+
+            selectedGear = gears[gearIndex];
+            selectedGearIndex = gearIndex;
+        }
+        public void UpGear()
+        {
+            ShiftGear(selectedGearIndex + 1);
+        }
+        public void DownGear()
+        {
+            ShiftGear(selectedGearIndex - 1);
         }
     }
 }
