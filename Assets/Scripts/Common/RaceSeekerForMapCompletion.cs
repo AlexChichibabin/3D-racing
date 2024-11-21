fcbe593e035ce866c68fcd6c6fd922f11c8556d6
AlexChichibabin <Wolfrahm57@mail.ru>
2024-11-21 18:40:43 +0300

    MapCompletion теперь может подгружать все треки через скрипт RaceSeekerForMapCompletion

--- Assets/Scripts/Common/RaceSeekerForMapCompletion.cs
+++ Assets/Scripts/Common/RaceSeekerForMapCompletion.cs
@@ -18,17 +18,18 @@ namespace Racing
 
             mapCompletion = GetComponent<MapCompletion>();
             GameObject[] allObjects = new GameObject[parent.childCount];
-
+            raceSpawner = new SpawnObjectByPropertiesList[allObjects.Length];
+            mapCompletion.SetCompletionInitialize(allObjects.Length, properties.Length);
 
             for (int i = 0; i < parent.childCount; i++)
             {
                 allObjects[i] = parent.GetChild(i).gameObject;
                 raceSpawner[i] = allObjects[i].GetComponent<SpawnObjectByPropertiesList>();
                 properties = raceSpawner[i].GetPropreties();
-                for (int j = 0; j< properties.Length; j++)
+                for (int j = 0; j < properties.Length; j++)
                 {
                     if(properties[j] is RaceInfo == true) 
-                        mapCompletion.SetCompletionData(properties[j] as RaceInfo);
+                        mapCompletion.SetCompletionData(properties[j] as RaceInfo, i, j);
                 }
             }
         }
--- Assets/Scripts/_imported/MapCompletion.cs
+++ Assets/Scripts/_imported/MapCompletion.cs
@@ -1,79 +1,92 @@
 using System;
+using System.Collections.Generic;
 using Racing;
 using UnityEngine;
 
-
-public class MapCompletion : MonoBehaviour, IDependency<LevelSequenceController>
+namespace Racing
 {
-    public const string m_FileName = "completion.dat";
-
-    [Serializable]
-    private class SeasonScore
+    public class MapCompletion : MonoBehaviour, IDependency<LevelSequenceController>
     {
+        public const string m_FileName = "completion.dat";
+
         [Serializable]
-        public class RaceScore
+        private class SeasonScore
         {
-            public RaceInfo race;
-            public int score;
-        }
-        public RaceScore[] racesInSeason;
-        public int score;
-    }
-
-    //[SerializeField] private GameObject[] m_Seasons;
-    [SerializeField] private SeasonScore[] m_CompletionDataPerSeason;
-    private LevelSequenceController levelSequenceController;
-    public void Construct(LevelSequenceController obj) => levelSequenceController = obj;
-    public int TotalScores { private set; get; }
-
-    private void Awake()
-    {
-        //DontDestroyOnLoad(gameObject);
-        Saver<SeasonScore[]>.TryLoad(m_FileName, ref m_CompletionDataPerSeason);
-        TotalScores = 0;
-        foreach (var score in m_CompletionDataPerSeason)
-        {
-            TotalScores += score.score;
-        }
-    }
-    public void SaveEpisodeResult(int levelScore)
-    {
-        foreach (var season in m_CompletionDataPerSeason)
-        {
-            season.score = 0;
-            foreach (var item in season.racesInSeason)
-            {   // Сохранение новых очков прохожения
-                if (item.race == levelSequenceController.CurrentRace)
-                {
-                    if (item.score < levelScore)
-                    {
-                        TotalScores += levelScore - item.score;
-                        item.score = levelScore;
-                        Saver<SeasonScore[]>.Save(m_FileName, m_CompletionDataPerSeason);
-                    }
-                    season.score += levelScore;
-                }
-            }
-            print($"Episode complete with score {levelScore}");
-        }
-            
-    }
-    public int GetEpisodeScore(RaceInfo m_Race)
-    {
-        foreach (var season in m_CompletionDataPerSeason)
-        {
-            foreach (var data in season.racesInSeason)
+            [Serializable]
+            public class RaceScore
             {
-                if (data.race == m_Race)
-                {
-                    return data.score;
-                }
+                public RaceInfo race;
+                public int score;
+            }
+            public RaceScore[] racesInSeason;
+            public int score;
+            /*public void RaceScoreInitialize(int Length) =>
+                racesInSeason = new RaceScore[Length];*/
+        }
+
+        [SerializeField] private SeasonScore[] m_CompletionDataPerSeason;
+        private LevelSequenceController levelSequenceController;
+        public void Construct(LevelSequenceController obj) => levelSequenceController = obj;
+        public int TotalScores { private set; get; }
+
+        private void Awake()
+        {
+            //DontDestroyOnLoad(gameObject);
+            //Saver<SeasonScore[]>.TryLoad(m_FileName, ref m_CompletionDataPerSeason);
+            TotalScores = 0;
+            foreach (var score in m_CompletionDataPerSeason)
+            {
+                TotalScores += score.score;
             }
         }
-        return 0;
-    }
-    public void SetCompletionData(RaceInfo race)
-    {
-
+        public void SaveEpisodeResult(int levelScore)
+        {
+            foreach (var season in m_CompletionDataPerSeason)
+            {
+                season.score = 0;
+                foreach (var item in season.racesInSeason)
+                {   // Сохранение новых очков прохожения
+                    if (item.race == levelSequenceController.CurrentRace)
+                    {
+                        if (item.score < levelScore)
+                        {
+                            TotalScores += levelScore - item.score;
+                            item.score = levelScore;
+                            //Saver<SeasonScore[]>.Save(m_FileName, m_CompletionDataPerSeason);
+                        }
+                        season.score += levelScore;
+                    }
+                }
+                print($"Episode complete with score {levelScore}");
+            }
+        }
+        public int GetEpisodeScore(RaceInfo m_Race)
+        {
+            foreach (var season in m_CompletionDataPerSeason)
+            {
+                foreach (var data in season.racesInSeason)
+                {
+                    if (data.race == m_Race)
+                    {
+                        return data.score;
+                    }
+                }
+            }
+            return 0;
+        }
+        public void SetCompletionInitialize(int SeasonsLength, int RacesLength)
+        {
+            m_CompletionDataPerSeason = new SeasonScore[SeasonsLength];
+            for (int i = 0; i < SeasonsLength; i++)
+            {
+                m_CompletionDataPerSeason[i] = new SeasonScore();
+                m_CompletionDataPerSeason[i].racesInSeason = new SeasonScore.RaceScore[RacesLength];
+            }
+        }
+        public void SetCompletionData(RaceInfo race, int currentSeason, int currentRace)
+        {
+            m_CompletionDataPerSeason[currentSeason].racesInSeason[currentRace] = new SeasonScore.RaceScore();
+            m_CompletionDataPerSeason[currentSeason].racesInSeason[currentRace].race = race;
+        }
     }
 }
